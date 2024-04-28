c21fc344de099678f46dea7e1977e1db81be52a3
Nathan Rahmavanya <143484750+UTOPI4T@users.noreply.github.com>
2024-04-28 20:40:38 +0700

    firebaseConfig

--- App.js
+++ App.js
@@ -12,13 +12,13 @@ import firebase from 'firebase/compat/app'
 import { initializeApp } from 'firebase/app';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAEB6pTlTj9_tjGWeWPvN5wxO6n6kaxcE8", 
-  authDomain: "inrush-authonly.firebaseapp.com", 
-  projectId: "inrush-authonly", 
-  storageBucket: "inrush-authonly.appspot.com", 
-  messagingSenderId: "479116863662", 
-  appId: "1:479116863662:web:308831712861f5cf26253f", 
-  measurementId: "G-GMC95BDDL7" 
+  apiKey: "yours", 
+  authDomain: "yours", 
+  projectId: "yours", 
+  storageBucket: "yours", 
+  messagingSenderId: "yours", 
+  appId: "yours", 
+  measurementId: "yours" 
 };
 
 const app = initializeApp(firebaseConfig);
