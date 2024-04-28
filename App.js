b49c0a2cf37bf43201edb8e223715da44232b74a
UTOPi4T <wsense911@gmail.com>
2024-04-28 18:55:26 +0700

    Login commit

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, {Component} from 'react'
 
 // Original auth
 import OriginalAuth from './matterOne/originalAuth'
 
 // Navigation
 import NavContainers from './navigations/NavContainers'
+
 // import * as firebase from 'firebase';
 import firebase from 'firebase/compat/app'
 import { initializeApp } from 'firebase/app';
@@ -23,15 +24,57 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 // const app = initializeApp(firebaseConfig);
-const App = () => {
-  return (
-     
-     <NavContainers/>
+
+export class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state ={
+      loaded: false,
+    }
+  }
+  componentDidMount(){
+    firebase.auth().onAuthStateChanged((user) => {
+      if(!user){
+        this.setState({
+          loggedIn: false,
+          loaded: true
+        })
+      } else {
+        this.setState({
+          loggedIn: true,
+          loaded: true
+        })
+      }
+    })
+  }
+
+  render() {
+    const {loggedIn, loaded} = this.state;
+    if(!loaded) {
+      return(
+        <View style={{flex:1, justifyContent:'center'}}>  
+          <Text> Loading...</Text>
+        </View>
+      )
+    }
+    if(!loggedIn){
+      return (
+        <NavContainers/>
+  
+  
+      )
+
+    }
+    return(
+      <View style={{flex:1, justifyContent:'center'}}>  
+        <Text> User is logged in</Text>
+      </View>
+    )
     
-  )
+  }
 }
 
-export default App
+
 
 const styles = StyleSheet.create({
   
--- screens/Auth/Login.js
+++ screens/Auth/Login.js
@@ -1,21 +1,96 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { Component } from 'react'
+import { View, StyleSheet, Text, Button } from 'react-native'
+import { TextInput } from 'react-native-gesture-handler';
+import firebase from 'firebase/compat/app';
+// import 'firebase/compat/auth';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
 
-const Login = () => {
-  return (
-    <View style={styles.container}>
-      <Text>Login</Text>
-    </View>
-  )
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAEB6pTlTj9_tjGWeWPvN5wxO6n6kaxcE8", 
+//   authDomain: "inrush-authonly.firebaseapp.com", 
+//   projectId: "inrush-authonly", 
+//   storageBucket: "inrush-authonly.appspot.com", 
+//   messagingSenderId: "479116863662", 
+//   appId: "1:479116863662:web:308831712861f5cf26253f", 
+//   measurementId: "G-GMC95BDDL7" 
+// };
+
+
+// if (!firebase.apps.length) {
+//   firebase.initializeApp(firebaseConfig);
+// }
+
+export class Login extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state ={
+      email:'',
+      password:'',
+    }
+    this.onSignUp =this.onSignUp.bind(this)
+  }
+  onSignUp(){
+    const {email, password} = this.state;
+    firebase.auth().signInWithEmailAndPassword(email, password)
+    .then((result) => {
+      console.log(result)
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={{backgroundColor:'#f1f1f1', padding:10, margin:5, }}>
+          <TextInput
+            placeholder='Username'
+            style={styles.txtInput}
+            onChangeText={(name) => this.setState({name})}
+
+          />
+          <TextInput
+            placeholder='email'
+            style={styles.txtInput}
+            onChangeText={(email) => this.setState({email})}
+
+          />
+          <TextInput
+            placeholder='password'
+            secureTextEntry={true}
+            style={styles.txtInput}
+            onChangeText={(password) => this.setState({password})}
+
+          />
+          </View>
+        <Button 
+          title='Login'
+          onPress={this.onSignUp}
+        />
+
+      </View>
+    )
+  }
 }
 
 export default Login
 
 const styles = StyleSheet.create({
-    container: {
-        flex:1,
-        justifyContent:'center',
-        alignItems:'center',
-        backgroundColor:'#709065',
-    }
+  container: {
+      flex:1,
+      justifyContent:'center',
+      // alignItems:'center',
+      backgroundColor:'#709065',
+  },
+  txtInput: {
+    borderBottomWidth:0.2,
+    padding:7,
+    margin:10,
+  }
+
 })
+
+
